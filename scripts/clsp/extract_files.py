b957c5f03332b73a1d4052a25293b9a1ee491c02
Steven Tan <wtan12@jhu.edu>
2021-11-02 17:25:02 -0400

    minor update

--- scripts/clsp/extract_files.py
+++ scripts/clsp/extract_files.py
@@ -13,10 +13,11 @@ def main():
 		if len(folder) < 1:
 			continue
 		lett_file = f'{kohen_data_prefix}/{folder}/v2.lett.xz'
-		tgt_dir = f'{output_dir}/{folder}'
-		if not os.path.exists(tgt_dir):
-			os.mkdir(tgt_dir)
-		shutil.copy(lett_file, tgt_dir)
+		folder_name = folder.replace('/', '_')
+		tgt_dir_name = f'{output_dir}/{folder_name}'
+		if not os.path.exists(tgt_dir_name):
+			os.mkdir(tgt_dir_name)
+		shutil.copy(lett_file, tgt_dir_name)
 
 
 if __name__ == '__main__':
