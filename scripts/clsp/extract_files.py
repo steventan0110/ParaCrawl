f46c321e9188236cb4d332f161e61dd1d342cb49
Steven Tan <wtan12@jhu.edu>
2021-11-02 19:57:07 -0400

    minor update

--- scripts/clsp/extract_files.py
+++ scripts/clsp/extract_files.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 
+
 def main():
 	kohen_data_prefix = '/home/pkoehn/statmt/data/site-crawl/data'
 	get_sent_file = '/home/wtan12/ParaCrawl/all-files-for-one-language.has-sent.ha'
@@ -15,9 +16,15 @@ def main():
 		lett_file = f'{kohen_data_prefix}/{folder}/v2.lett.xz'
 		folder_name = folder.replace('/', '_')
 		tgt_dir_name = f'{output_dir}/{folder_name}'
+		copied_file_name = f'{output_dir}/{folder_name}/v2.lett.xz'
+		if os.path.exists(copied_file_name):
+			continue
 		if not os.path.exists(tgt_dir_name):
 			os.mkdir(tgt_dir_name)
-		shutil.copy(lett_file, tgt_dir_name)
+		try:
+			shutil.copy(lett_file, tgt_dir_name)
+		except:
+			continue
 
 
 if __name__ == '__main__':
