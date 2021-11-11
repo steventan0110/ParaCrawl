c2eafbd1cd03ecbdf0c0b869115c088a5c528512
Steven Tan <wtan12@jhu.edu>
2021-11-11 00:08:49 -0500

    update script for laser score learning

---
+++ scripts/clsp/laser_score.sh
@@ -0,0 +1,43 @@
+ROOT=${HOME}/ParaCrawl
+dir=/export/b02/wtan/dataset/lett
+sent_ha=${dir}/sent.en-ha.ha.dedup
+sent_en=${dir}/sent.en-ha.en.dedup
+output=${dir}/sent.en-ha.laser
+
+export LASER=${HOME}/ParaCrawl/scripts/paracrawl/LASER
+export LASER_SCORING=${HOME}/ParaCrawl/scripts/paracrawl/laser-scoring
+model_dir="${LASER}/models"
+encoder="${model_dir}/bilstm.93langs.2018-12-26.pt"
+bpe_codes="${model_dir}/93langs.fcodes"
+
+conda activate crawl
+
+Embed () {
+  ll=$1
+  txt=$2
+  embed=$3
+
+  if [ ! -s ${embed} ] ; then
+    cat ${txt} | python3 ${LASER}/source/embed.py \
+      --encoder ${encoder} \
+      --token-lang ${ll} \
+      --bpe-codes ${bpe_codes} \
+      --output ${embed} \
+      --verbose
+   fi
+}
+
+Embed en ${sent_en} ${sent_en}.embed
+Embed ha ${sent_ha} ${sent_ha}.embed
+
+
+python3 ${LASER}/source/mine_bitexts.py \
+    ${sent_en} ${sent_ha} \
+    --src-lang en --trg-lang ha \
+    --src-embeddings ${sent_en}.embed \
+    --trg-embeddings ${sent_ha}.embed \
+    --mode score --retrieval max --margin ratio -k 4  \
+    --output ${output} --verbose --gpu --unify
+
+# python $ROOT/scripts/paracrawl/laser_score.py --file1 ${sent_en} --file2 ${sent_ha} -o ${output}
+
--- scripts/paracrawl/laser_score.py
+++ scripts/paracrawl/laser_score.py
@@ -1,20 +1,27 @@
 from LASER.source import embed
 import argparse
 
-def main():
+
+def main(args):
+	model_dir = '/home/steven/Code/GITHUB/ParaCrawl/scripts/paracrawl/LASER/models'
+	print(args.file1)
+
 	# use toy sentence as test first
 	ha = 'Sunan yarinyar Wangari.'
 	en = 'Tiv people love animals but they burn bushes to kill rats.'
 	# use laser embed
-	model_dir = '/home/steven/Code/GITHUB/ParaCrawl/scripts/paracrawl/LASER/models'
+
 	encoder = embed.SentenceEncoder(f'{model_dir}/bilstm.93langs.2018-12-26.pt',
 	                                max_sentences=10,
-		                            max_tokens=10,
-				                    sort_kind='quicksort',
-				                    cpu=True)
+	                                max_tokens=10,
+	                                sort_kind='quicksort',
+	                                cpu=True)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Use Laser to output a bitext datasets')
-    parser.add_argument('-o', '--output', required=True, default='.', type=str, help='Output directory for the dataset')
-    parser.add_argument('-i', '--input', required=True, default='.', type=str)
-    args = parser.parse_args()
-    main()
+	parser = argparse.ArgumentParser(description='Use Laser to output a bitext datasets')
+	parser.add_argument('-o', '--output', required=True, default='.', type=str, help='Output directory for the dataset')
+	parser.add_argument('--file1', required=True, default='.', type=str)
+	parser.add_argument('--file2', required=True, default='.', type=str)
+	args = parser.parse_args()
+	main(args)
